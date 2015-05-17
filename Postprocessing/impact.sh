f34a8211281c318dbe96fc64246bd419411d76d4
Romain <romain.chailan@gmail.com>
2015-05-17 14:52:26 +0200

    use lambert93 projection to have cartesian distance

--- Postprocessing/impact.sh
+++ Postprocessing/impact.sh
@@ -15,10 +15,13 @@ bathy="inputs/sirocco.europe.grd"
 envelope="-R3/5/42.25/43.60"
 envelopelon="-R3/5"
 envelopelat="-R42/44"
-
+envelopelambert93="-R700000.0000/865629.1328/6128081.8176/6279561.7280"
 outfile="outputs/impact"
+################
 
-# GMT PARAMS #
+################
+## GMT PARAMS ##
+################
 png_resolution=800
 projection=-JM20c
 gmt gmtset PROJ_ELLIPSOID Clarke-1880-IGN
@@ -33,6 +36,23 @@ gmt gmtset FONT_LABEL 8p
 gmt gmtset MAP_LABEL_OFFSET 0.2c
 gmt gmtset FORMAT_FLOAT_OUT %8.8f
 ###############
+
+########################
+## Projection  params ##
+########################
+lonREF=3.0
+latREF=46.5
+latsec1=44.0
+latsec2=49.0
+offsetX=700000
+offsetY=6600000
+projL=-Jl${lonREF}/${latREF}/${latsec1}/${latsec2}
+paramJ1=${projL}/1:1
+paramJ2=-Jl${lonREF}/${latREF}/${latsec1}/${latsec2}/1:850000
+offset=-C${offsetX}/${offsetY}
+projX=-Jx1:850000
+########################
+
 
 
 #####################################
@@ -55,19 +75,18 @@ if [ "${HASBASELINE}" = false ] ; then
 			rm ${work}/bathy-neg.xy ${work}/bathy-neg.z ${work}/bathy-neg.xyz ${work}/bathy.z
 			log $? "clean tmp bathy files"
 
+			gmt mapproject ${work}/bathy.xyz $paramJ1 -Rg -F -S  $offset > ${work}/bathy-lamb93.xyz
+			log $? "mapproject bathy"
+
 			isobathmin=$(awk "BEGIN {print $H0-0.1; exit}")
 			isobathmax=$(awk "BEGIN {print $H0+0.1; exit}")
-			gmt gmtselect ${envelope} -Z${isobathmin}/${isobathmax} ${work}/bathy.xyz > ${work}/isobath.xyz
+			
+			# gmt gmtselect ${envelope} -Z${isobathmin}/${isobathmax} ${work}/bathy.xyz > ${work}/isobath.xyz
+			envelope=${envelopelambert93}
+			gmt gmtselect ${envelope} -Z${isobathmin}/${isobathmax} ${work}/bathy-lamb93.xyz > ${work}/isobath.xyz
 			log $? "iso-value point"
 
-			envelopelon=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} {print $1"/"$2 }' )
-			envelopelat=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} {print "-R"$3"/"$4 }' )
-			# awk '{print $2,$1}' ${work}/isobath.xyz > ${work}/isobath.yx
-			# awk '{print $1,$2}' ${work}/isobath.xyz > ${work}/isobath.xy
-			# gmt greenspline ${work}/isobath.xy ${envelopelon} -I${EQUIDIST} -Cn15 -St0.95 -G${work}/baseline_splineX 
-			# gmt greenspline ${work}/isobath.yx ${envelopelat} -I${EQUIDIST} -Cn15 -St0.95 -G${work}/baseline_splineY 
-
-			gmt blockmean ${work}/isobath.xyz ${envelope} -I20k -V > ${work}/isobathblockmean.xyz
+			gmt blockmean ${work}/isobath.xyz ${envelope} -I${BLOCKMEANINC} > ${work}/isobathblockmean.xyz
 			log $? "blockmean isobath"
 			sort ${work}/isobathblockmean.xyz > ${work}/isobathblockmean-sorted.xyz
 
@@ -77,6 +96,7 @@ if [ "${HASBASELINE}" = false ] ; then
 			awk '{print NR,$1}' ${work}/isobathblockmean-sorted.xyz > ${work}/isobath.ix
 			awk '{print NR,$2}' ${work}/isobathblockmean-sorted.xyz > ${work}/isobath.iy
 			max=$(( $(wc -l ${work}/isobathblockmean-sorted.xyz | awk '{print $1}') ))
+
 			min=1
 			# nb=200
 			nbpersec=10
@@ -85,13 +105,30 @@ if [ "${HASBASELINE}" = false ] ; then
 			envelopei=-R${min}/${max}
 			gmt greenspline ${work}/isobath.iy ${envelopei} -I${inc} -St0.95 -G${work}/baseline_splineY
 			gmt greenspline ${work}/isobath.ix ${envelopei} -I${inc} -St0.95 -G${work}/baseline_splineX
-
-			# gmt greenspline ${work}/isobath.xyz ${envelopelon} -I0.01 -Cn10 -St0.95 -G${work}/baseline_spline -V
-			log $? "greenspline"
-
-			# gmt grdtrack ${work}/isobath.xyz -Ar -C0.1/0.01/0.05 -G${bathy} ${envelope}| awk '$0 ~ /^>/ {print $7}' | sed 's/\// /g' > ${work}/track.xyz
-			# log $? "grdtrack"
 			join ${work}/baseline_splineX ${work}/baseline_splineY | awk '{print $2,$3}' > ${work}/baseline_spline
+
+			# envelopelon=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} {print $1"/"$2 }' )
+			# envelopelat=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} {print "-R"$3"/"$4 }' )
+
+			# minX=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} { print $1 }' |sed 's/-R//')
+			# maxX=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} { print $2 }' )
+			# minY=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} { print $3 }' )
+			# maxY=$(gmt minmax -I0.001 ${work}/isobath.xyz |awk 'BEGIN {FS="/"} { print $4 }'  )
+
+			# nb=100
+			# incX=$(echo "($maxX-$minX) / $nb" | bc -l)
+			# incY=$(echo "($maxY-$minY) / $nb" | bc -l)
+			
+			# awk '{print $2,$1}' ${work}/isobath.xyz > ${work}/isobath.yx
+			# awk '{print $1,$2}' ${work}/isobath.xyz > ${work}/isobath.xy
+			# gmt greenspline ${work}/isobath.xy ${envelopelon} -I${incX} -Cn15 -St0.95 -G${work}/baseline_splineX 
+			# gmt greenspline ${work}/isobath.yx ${envelopelat} -I${incY} -Cn15 -St0.95 -G${work}/baseline_splineY 
+
+			# log $? "greenspline"
+
+			# # gmt grdtrack ${work}/isobath.xyz -Ar -C0.1/0.01/0.05 -G${bathy} ${envelope}| awk '$0 ~ /^>/ {print $7}' | sed 's/\// /g' > ${work}/track.xyz
+			# # log $? "grdtrack"
+			# paste ${work}/baseline_splineX ${work}/baseline_splineY | awk '{print $4,$3}' > ${work}/baseline_spline
 
 			log $? "baseline created";;
 		3 )
@@ -110,23 +147,6 @@ fi
 ##########################
 if [ "${HASPROFILE}" = false ]; then
 	log "notice" "No profile found."
-	# gmt grdtrack ${work}/baseline_spline -C${PROFILELENGTH}/${EQUIPROFILEDS}/${EQUIPROFILESPACE} -Ar  -G${bathy} ${envelope} > ${work}/profiles
-	# log $? "grdtrack"
-
-	# cat  ${work}/profiles |awk '$1 ~ /^>/ { print $0 }' > ${work}/profilesheader
-	# log $? "save profiles header"
-
-	# echo ">"  >> ${work}/profiles
-	# countfull=$(wc -l ${work}/profilesheader |awk '{print $1}')
-	# count=$(( ${countfull} - 1 ))
-	# csplit -s -n 5 -f ${work}/profiletrack  ${work}/profiles "/>/" "{${count}}" 
-	# log $? "csplit files"
-	# rm ${work}/profiletrack00${countfull}
-
-	# for profile in $(ls ${work}/profiletrack*) ; do
-	# 	awk '$1 ~ !/^>/ {if ($5 >= -60.1 && $5 <= 1 && $5 != NaN ) print $1,$2,$5 }' ${profile}  > ${profile}-points
-	# 	rm ${profile}
-	# done
 
 	normale="${work}/normale"
 	profiletrack="${work}/profiletrack"
@@ -175,6 +195,22 @@ if [ "${HASPROFILE}" = false ]; then
 	nbrpprof=`awk 'END { print NR }' ${work}/profiletrack1`
 fi
 
+##########################
+## Shoreline extraction ##
+##########################
+if [ "${HASSHORELINE}" = false ]; then
+	# gmt gshhg ${PATHTOGSHHGBIN} -I0 -N1 > ${input}/coast.dat
+	log $? "extract shoreline to ascii file"
+
+	awk '$1 ~ /^[0-9]/ {print $0}' ${input}/coast.dat > ${work}/coast.xy
+	log $? "make it continuous file"
+
+	gmt mapproject ${work}/coast.xy $paramJ1 -Rg -F -S  $offset > ${work}/coast-lambert93.xy
+	log $? "mapproject shoreline"	
+fi
+
+
+
 ###########################
 ## Geometry computations ##
 ###########################
@@ -192,7 +228,7 @@ if [ "${HASGEOMETRY}" = false ]; then
 		gmt grdtrack $profile -hi1 -G${bathy} -sa > ${profile}-tmp
 		log $? "grdtrack profile points"
 
-		awk '$3 > max {max=$3; maxline=$0}; END {print maxline}' ${profile}-tmp > ${work}/tmp_coastpoint
+		awk '$3 < min {min=$3; minline=$0}; END {print minline}' ${profile}-tmp > ${work}/tmp_coastpoint
 	    z=$(awk '{print $3}' ${work}/tmp_coastpoint)
 	    echo "$profilenb,$z"
 	    if [ "$z" == "" ]; then 
@@ -221,9 +257,9 @@ fi
 #########################
 ## Extract storms data ##
 #########################
-if [ "${HASTOEXTRACT}" = true ]; then
+if [ "${HASEXTRACTED}" = false ]; then
 	# Interpolation of vars to the (xi,yi) points, i.e. the baselines points
-	storms=$(ls /storm*.nc)
+	storms=$(ls ${STORMSDIR}/storm*.nc)
 	vars=${VARIABLES}
 	# vars="hs_uplifted "
 	flux=${storms[0]}
@@ -283,129 +319,86 @@ fi
 
 
 
-
-
-
-
-# On calcul l'impact en (xi,yi) sur baseline
-
-#                             #
-# Calcul des indices d'impact #
-#                             #
-
-# nbrbouees=`awk 'END {print NR}' ./input/bouee.txt`
-# nbrregimes=`awk 'END {print NR}' ./input/regime_vague.txt`
-# nbbouee=$(($nbrbouees-1))
-# nbregime=$(($nbrregimes-1))
-
-
-# myimpactelementaire="./garbage/impact_elementaire" # numzone, numbouee, numregime, Ikji
-# myimpactbouee="./garbage/impact_bouee"             # numzone, numbouee, Iji
-# myimpactregime="./garbage/impact_regime"           # numzone, numregime, Iki
-# myimpactglobal="./output/impact_global"            # numzone, Ii
-
-# for i in `seq 2 $nbrprofiles`
-#     do Sj=0
-#        Sdbc=0
-#        xi=`awk 'NR == '$i' {print $2}' $normale`
-#        yi=`awk 'NR == '$i' {print $3}' $normale`
-#        xo=`awk 'NR == '$i' {print $4}' $normale`
-#        yo=`awk 'NR == '$i' {print $5}' $normale`
-#        dio=`echo "sqrt(($xi - $xo)*($xi - $xo) + ($yi - $yo)*($yi - $yo))" |bc -l`
-#        coslambda=`echo "($yo - $yi)/$dio" |bc -l`
-#        sinlambda=`echo "($xi - $xo)/$dio" |bc -l`
-#        numzone=$(($i-1))
-#     echo $numzone
-#        mytemp="./garbage/impact_temp_$numzone"
-#        for j in `seq 2 $nbrbouees`
-#            do Sk=0
-#               xb=`awk 'NR == '$j' {print $1}' ./garbage/bouee_lambert`
-#               yb=`awk 'NR == '$j' {print $2}' ./garbage/bouee_lambert`
-#               dbc=`echo "sqrt(($xi - $xb)*($xi - $xb) + ($yi - $yb)*($yi - $yb))" |bc -l` # distance bouée-côte
-#               cosgamma=`echo "($yi - $yb)/$dbc" |bc -l`
-#               singamma=`echo "($xb - $xi)/$dbc" |bc -l`
-#               numbouee=$(($j-1))
-#               nregime=0
-#               for k in `seq 2 $nbrregimes`
-#                   do betameteo=`awk 'NR == '$k' {print $1}' ./input/regime_vague.txt`
-#                      numpi=3.1415926535897932384626433832795
-#                      cosbeta=`echo "c((180 - $betameteo)*$numpi/180)" |bc -l`
-#                      sinbeta=`echo "s((180 - $betameteo)*$numpi/180)" |bc -l`
-#                      numregime=$(($k-1))
-#                      cbouee=$(($numbouee+1))
-#                      pbouee=`awk 'NR == '$k' {print$'$cbouee'}' ./input/regime_vague.txt`
-#                      if [ $pbouee == 0 ]
-#                         then cosalpha=0
-#                              cosomega=0
-#                         else cosalpha=`echo "$singamma*$sinbeta + $cosgamma*$cosbeta" |bc -l`
-#                              cosomega=`echo "$sinlambda*$sinbeta + $coslambda*$cosbeta" |bc -l`
-#                              nregime=$(($nregime+1))
-#                      fi
-#                      if [ $(echo "$cosalpha < 0" |bc) = 1 ]
-#                         then cosalpha=0
-#                      fi
-#                      if [ $(echo "$cosomega < 0" |bc) = 1 ]
-#                         then cosomega=0
-#                      fi
-#                      Ikji=`echo "100*$cosalpha*$cosomega" |bc -l` # Impact élémentaire du régime k à la bouée j sur la zone i
-#                      awk 'BEGIN { printf("%d %d %d %f\n",'$numzone','$numbouee','$numregime','$Ikji') }' >> $myimpactelementaire
-#                      Sk=`echo "$Sk + $Ikji" |bc -l` # somme sur les k de Ikji
-#                      Itemp=`echo "$Ikji/$dbc" |bc -l`
-#                      awk 'BEGIN { printf("%d %d %d %f %f %d\n",'$numzone','$numbouee','$numregime','$Itemp','$dbc','$pbouee') }' >> $mytemp
-#               done
-#               Iji=`echo "$Sk/$nregime" |bc -l` # impact de tous les régime à la bouée j sur i
-#               awk 'BEGIN { printf("%d %d %f\n",'$numzone','$numbouee','$Iji') }' >> $myimpactbouee
-#               Sj=`echo "$Sj + $Iji/$dbc" |bc -l` # somme sur les j de Iji pondérée par 1/dbc
-#               Sdbc=`echo "$Sdbc + (1/$dbc)" |bc -l` # somme sur les j des 1/dbc
-#        done
-#        Ii=`echo "$Sj/$Sdbc" |bc -l` # impact de tous les régime à toutes les bouées sur i
-#        awk 'BEGIN { printf("%d %f\n",'$numzone','$Ii') }' >> $myimpactglobal
-#        for k in `seq 1 $nbregime`
-#            do awk 'BEGIN { I=0 ; D=0 ;
-#                          }
-#                    $3 == '$k' { I = I + $4 ; D = D + $6/$5 } 
-#                    END { Iki = I/D ;
-#                          printf("%d %d %f\n",'$numzone','$k',Iki) 
-#                        }' $mytemp >> $myimpactregime
-#        done
-# done
-
 ################
 ## GMT PLOTS  ##
 ################
 if [ "${GMT}" = true ]; then
 
-	palette="work/shallow-water.cpt"
-	gmt makecpt -Z-500/0/50 -Csealand -G-5000/0 -M > $palette
-	gmt	grdimage ${bathy} $projection $envelope -C$palette -P -K > ${outfile}.ps
-	log $? "grdimage"
-	gmt pscoast $projection $envelope -Df -G#d9bb7a -Cwhite -N1/0.2p,#0000FF,solid  -P -K -O >> ${outfile}.ps
-	log $? "pscoast"
-	gmt	psbasemap $envelope $projection $echelle $rose -Bf0.5a1:longitude:/f0.25a0.5:latitude:/:."bla":WeSn -P -O -K >> ${outfile}.ps
-	log $? "psbasemap"
-	gmt	grdcontour $bathy $envelope -S $projection -C$contourfile -A60+gwhite+f4 -Wcthinnest,black,solid -Wathinner,black,solid -P -O -K >> ${outfile}.ps
-	log $? "grdcontour"
+	MAPPROJECT=true
+	
+	if [ "${MAPPROJECT}" = true ]; then
+		palette="work/shallow-water.cpt"
+		envelope="-R3/5/42.25/43.60"
+		# gmt makecpt -Z-5000/0/50 -Csealand -G-5000/0 -M > $palette		
+		# gmt makecpt -Z0/2000/50 -C -G0/2000 -M > $palette
+		gmt xyz2grd ${work}/bathy-lamb93.xyz $envelopelambert93 -G${work}/bathy-lamb93.grd -I832/832
+		log $? "xyz2grd"
+		gmt grd2cpt ${work}/bathy-lamb93.grd -Z > $palette
+		log $? "grd2cpt"
 
-	gmt psxy ${work}/isobath.xyz $projection $envelope -S+0.4c -W1p,red -P -O -K >> ${outfile}.ps
-	gmt psxy ${work}/baseline_spline $projection $envelope -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
+		gmt	grdimage -V ${work}/bathy-lamb93.grd $projX -C$palette -P -K > ${outfile}.ps
+		log $? "grdimage"
+		gmt pscoast -V ${paramJ2} ${envelope} -Df -G#d9bb7a -Cwhite -N1/0.2p,#0000FF,solid  -P -K -O >> ${outfile}.ps
+		log $? "pscoast"
+		gmt	psbasemap -V ${paramJ2} ${envelope} $echelle $rose -Bf0.5a1:longitude:/f0.25a0.5:latitude:/:."bla":WeSn -P -O -K >> ${outfile}.ps
+		log $? "psbasemap"
+		gmt	grdcontour -V ${work}/bathy-lamb93.grd $projX ${envelopelambert93} -S -A60+gwhite+f4 -Wcthinnest,black,solid -Wathinner,black,solid -P -O -K >> ${outfile}.ps
+		log $? "grdcontour"
 
-	if [ -f ${work}/isobathblockmean.xyz ] ; then 
-		gmt psxy ${work}/isobathblockmean.xyz $projection $envelope -S+0.4c -W1p,blue -P -O -K >> ${outfile}.ps
+		gmt psxy -V ${work}/isobath.xyz ${projX} ${envelopelambert93} -S+0.4c -W1p,red -P -O -K >> ${outfile}.ps
+		gmt psxy ${work}/baseline_spline ${projX} ${envelopelambert93} -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
+		# gmt psxy ${work}/coast-lambert93.xy ${projX} ${envelopelambert93} -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
+
+		if [ -f ${work}/isobathblockmean.xyz ] ; then 
+			gmt psxy ${work}/isobathblockmean.xyz ${projX} ${envelopelambert93} -S+0.4c -W1p,blue -P -O -K >> ${outfile}.ps
+		fi
+
+		# #gmt psxy ${work}/track.xyz $projection $envelope -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
+		for profile in $(ls ${work}/profiletrack*) ; do
+			tail -n +2 $profile > ${work}/profiletmp
+			gmt psxy ${work}/profiletmp ${projX} ${envelopelambert93} -Sp0.1c -W0.5p,brown -P -O -K >> ${outfile}.ps
+		done
+		log $? "psxy"
+
+		gmt	psscale -D21/9/17.5/0.3 -C$palette -B500:"":/:"Depth(m)": -E -O  >> ${outfile}.ps
+		log $? "psscale"
+
+		gmt ps2raster -E$png_resolution -A -Tg -P ${outfile}.ps
+		log $? "psraster"
+		rm ${outfile}.ps
+	else
+		palette="work/shallow-water.cpt"
+		gmt makecpt -Z-500/0/50 -Csealand -G-5000/0 -M > $palette
+		gmt	grdimage ${bathy} $projection $envelope -C$palette -P -K > ${outfile}.ps
+		log $? "grdimage"
+		gmt pscoast $projection $envelope -Df -G#d9bb7a -Cwhite -N1/0.2p,#0000FF,solid  -P -K -O >> ${outfile}.ps
+		log $? "pscoast"
+		gmt	psbasemap $envelope $projection $echelle $rose -Bf0.5a1:longitude:/f0.25a0.5:latitude:/:."bla":WeSn -P -O -K >> ${outfile}.ps
+		log $? "psbasemap"
+		gmt	grdcontour $bathy $envelope -S $projection -C$contourfile -A60+gwhite+f4 -Wcthinnest,black,solid -Wathinner,black,solid -P -O -K >> ${outfile}.ps
+		log $? "grdcontour"
+
+		gmt psxy ${work}/isobath.xyz $projection $envelope -S+0.4c -W1p,red -P -O -K >> ${outfile}.ps
+		gmt psxy ${work}/baseline_spline $projection $envelope -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
+
+		if [ -f ${work}/isobathblockmean.xyz ] ; then 
+			gmt psxy ${work}/isobathblockmean.xyz $projection $envelope -S+0.4c -W1p,blue -P -O -K >> ${outfile}.ps
+		fi
+
+		# gmt psxy ${work}/track.xyz $projection $envelope -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
+		for profile in $(ls ${work}/profiletrack*) ; do
+			tail -n +2 $profile > ${work}/profiletmp
+			gmt psxy ${work}/profiletmp $projection $envelope -Sp0.1c -W0.5p,brown -P -O -K >> ${outfile}.ps
+		done
+		log $? "psxy"
+
+		gmt	psscale -D21/9/17.5/0.3 -C$palette -B500:"":/:"Depth(m)": -E -O  >> ${outfile}.ps
+		log $? "psscale"
+
+		gmt ps2raster -E$png_resolution -A -Tg -P ${outfile}.ps
+		log $? "psraster"
+		rm ${outfile}.ps
 	fi
-
-	# gmt psxy ${work}/track.xyz $projection $envelope -S+0.4c -W1p,black -P -O -K >> ${outfile}.ps
-	for profile in $(ls ${work}/profiletrack*) ; do
-		tail -n +2 $profile > ${work}/profiletmp
-		gmt psxy ${work}/profiletmp $projection $envelope -Sp0.1c -W0.5p,brown -P -O -K >> ${outfile}.ps
-	done
-	log $? "psxy"
-
-	gmt	psscale -D21/9/17.5/0.3 -C$palette -B500:"":/:"Depth(m)": -E -O  >> ${outfile}.ps
-	log $? "psscale"
-
-	gmt ps2raster -E$png_resolution -A -Tg -P ${outfile}.ps
-	log $? "psraster"
-	rm ${outfile}.ps
 fi
 
 rightnow
--- Postprocessing/properties.sh
+++ Postprocessing/properties.sh
@@ -3,27 +3,26 @@
 ############################
 ## BASELINE CONFIGURATION ##
 ############################
-export HASBASELINE=false
+export HASBASELINE=true
 # mode 1 : h0 max from  => max[Tp(t)] => T_0^max => L_0^max => h0^max
 # mode 2 : h0 is fixed at a reference value
 # mode 3 : h0 ~ 10-20m assuming WW3 is correct close to the littoral
 export BASELINEMODE=2
 export H0=60
-# In degrees for the moment
-export EQUIDIST=0.01 
+export BLOCKMEANINC=18000
 
 ######################################
 ## NORMAL to BASELINE CONFIGURATION ##
 ######################################
-export HASPROFILE=true
+export HASPROFILE=false
+export ORTHOLENGTH=25000
+export ORTHONBPOINTS=80
 
-# following three dist needs to be in the same unit (see -C grdtrack)
-#export PROFILELENGTH=70k
-#export EQUIPROFILEDS=0.1k
-#export EQUIPROFILESPACE=2k
-
-export ORTHOLENGTH=0.5
-export ORTHONBPOINTS=30
+##########################
+## Shoreline extraction ##
+##########################
+export HASSHORELINE=true
+export PATHTOGSHHGBIN=/Users/rchailan/Applications/gshhg/gshhg-bin-2.3.4/gshhs_f.b
 
 ###########################
 ## Geometry computations ##
@@ -33,7 +32,7 @@ export HASGEOMETRY=true
 #########################
 ## Extract storms data ##
 #########################
-export HASTOEXTRACT=false
+export HASEXTRACTED=true
 export NEARNEIGHBORINC=0.01
 export NEARNEIGHBORS=5k
 export NEARNEIGHBORN=4/2
@@ -44,4 +43,4 @@ export VARIABLES="hs_uplifted dir fp"
 ################
 ## GMT PLOTS  ##
 ################
-export GMT=true
+export GMT=false
