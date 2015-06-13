61bccf3cda111fcf996633163af2271f9be6b6b2
Romain <romain.chailan@gmail.com>
2015-06-13 10:06:52 +0200

    Alternative to optim likelihood

--- deHaan-sp/R/scripts/libs/testlikelihood.R
+++ deHaan-sp/R/scripts/libs/testlikelihood.R
@@ -20,47 +20,82 @@ if (data_load) {
 # res<-sp.fit(data = d$var, threshold = q, cmax = TRUE, r = 3, itnmax = 2000, start=theta, optimfn="optimx")
 
 
-# Simulation gev
-n<-1000
-theta<-c(1,3,0.4) #marche
-theta<-c(0,3,0.4) #plante !
-d<-rsp(n,theta)
 
-amax<-function(theta,x) {
+
+amin<-function(theta,x) {
   if (theta[2] < 1*10^(-4) | theta[3] == 0) {
-    return (-9999999)
+    return (9999999)
   } else {
     c <- theta[1]-theta[2]/theta[3]
     coef <- ( (1/theta[3]) + 1 )
     n <- length(x)
     val <- 0
+    j<-0
     if (theta[3] > 0) {
       for (i in 1:n) {
-        if (x[i] > c) 
-        {val <- val+log(1 + theta[3] * ((x[i]-theta[1])/theta[2]))}
+        if (x[i] > c) {
+          val <- val+log(1 + theta[3] * ((x[i]-theta[1])/theta[2]))
+        }
+        j<-j+1
       }
     } else if (theta[3] < 0) {
       for (i in 1:n) {
-        if (x[i] < c ) 
-        {val <- val+log(1 + theta[3] * ((x[i]-theta[1])/theta[2]))}
+        if (x[i] < c ) {
+          val <- val+log(1 + theta[3] * ((x[i]-theta[1])/theta[2]))
+          j<-j+1
+        }
       }
-    }    
-    if (val != 0) {
-      l<-n * log(theta[2])  + coef*val
-      print(paste("likelihood amax:",l,"c:",c,"mu:",theta[1],"sigma:",theta[2],"xi:",theta[3]))
-      return (-n * log(theta[2])  - coef*val)
+    }
+    if (j>800) {
+      l<-n * log(theta[2]) + coef*val
+      print(paste("nllh",l,"c:",c,"mu:",theta[1],"sigma:",theta[2],"xi:",theta[3],"j",j))
+      return (n * log(theta[2]) + coef*val)
     } else {
-      print(paste("likelihood amax:",-99999999,"c:",c,"mu:",theta[1],"sigma:",theta[2],"xi:",theta[3]))
-      return (-9999999)
+      print(paste("nllh:",99999999,"c:",c,"mu:",theta[1],"sigma:",theta[2],"xi:",theta[3],"j",j))
+      return (9999999) 
     }
   } 
 }
 
-res<-NULL
-res<-optim(par = c(0,1,1),fn = amax,x=d,hessian = F, lower=c(-Inf,1*10^(-4),-Inf),upper=c(min(d),Inf,Inf),
-           control = list(fnscale=-1),method="L-BFGS-B")
-print(theta)
-print(res)
+amin2<-function(theta,x) {
+  if (theta[2] < 1*10^(-4) | theta[3] == 0) {
+    return (9999999)
+  } else {
+    c <- theta[1]-theta[2]/theta[3]
+    coef <- ( (1/theta[3]) + 1 )
+    n <- length(x)
+    val <- 0
+    j<-0
+    if ((theta[3] > 0) & (theta[3] < 1) & (min(x)>theta[1])){
+      for (i in 1:n) {
+        if (x[i] > c){ 
+          val <- val+log(1 + theta[3] * ((x[i]-theta[1])/theta[2])) 
+        }
+        j<-j+1
+      }
+    } else if ((theta[3] < 0) & (theta[3] > - 1) & (min(x)>theta[1])) {
+      for (i in 1:n) {
+        if ((x[i] < c) ) {
+          val <- val+log(1 + theta[3] * ((x[i]-theta[1])/theta[2]))
+          j<-j+1
+        }
+      }
+    }
+    if (j>800) {
+      l<-n * log(theta[2]) + coef*val
+      print(paste("nllh",l,"c:",c,"mu:",theta[1],"sigma:",theta[2],"xi:",theta[3],"j",j))
+      return (n * log(theta[2]) + coef*val)
+    } else {
+      print(paste("nllh:",99999999,"c:",c,"mu:",theta[1],"sigma:",theta[2],"xi:",theta[3],"j",j))
+      return (9999999)
+    }
+  }
+}
 
-# res<-optimx(par = c("mu"=0,"sigma"=1*10^(-4),"xi"=1),fn = amax,x=d,hessian = F, lower=c(Inf,1*10^(-4),-1),
-#             control=list(all.methods=TRUE, save.failures=FALSE, trace=0,maximize=TRUE))
+n<-1000
+theta1<-c(1,3,-0.1)
+d1<-rsp(n,theta1)
+
+res1<-nlminb(start = c(min(d1),1,0.5),objective = amin,x=d1,hessian = F, lower=c(-Inf,1*10^(-4),-1),upper=c(min(d1),Inf,1),control = list(iter.max=200))
+res1bis<-optim(par = c(min(d1),1,0.5),fn = amin,x=d1,hessian = F, lower=c(-Inf,1*10^(-4),-1),upper=c(min(d1),Inf,1),method="L-BFGS-B")
+res1bisbis<-optim(par = c(min(d1),1,0.5),fn = amin2,x=d1,hessian = F,method="BFGS")
