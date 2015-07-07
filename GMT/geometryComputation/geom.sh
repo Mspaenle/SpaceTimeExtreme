d08ca4c5443610fe69431e34387f652a01c62d6e
Romain <romain.chailan@gmail.com>
2015-07-07 10:53:41 +0200

    possibility to select size from computational mesh and display nodename

---
+++ GMT/geometryComputation/geom.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#source utilities
+source ./resources/sh/utility.sh
+rightnow
+log "notice" "STARTING... $d"
+
+## PARAMETERS ##
+work="work"
+output="output"
+outfile=$output/sites-info.dat
+sites=input/sites.xyz
+
+
+################
+## GMT PARAMS ##
+################
+png_resolution=800
+projection=-JM20c
+gmt gmtset PS_MEDIA a3
+gmt gmtset MAP_ANNOT_ORTHO ver_text
+gmt gmtset MAP_FRAME_TYPE fancy+
+gmt gmtset MAP_FRAME_WIDTH 0.08c
+gmt gmtset FONT_TITLE Helvetica
+gmt gmtset FONT_TITLE 14p
+gmt gmtset MAP_TITLE_OFFSET 0.3c
+gmt gmtset FONT_LABEL 8p
+gmt gmtset MAP_LABEL_OFFSET 0.2c
+gmt gmtset FORMAT_FLOAT_OUT %8.8f
+gmt gmtset PROJ_ELLIPSOID Clarke-1880-IGN
+gmt gmtset PROJ_LENGTH_UNIT c
+###############
+
+########################
+## Projection  params ##
+########################
+lonREF=3.0
+latREF=46.5
+latsec1=44.0
+latsec2=49.0
+offsetX=700000
+offsetY=6600000
+projL=-Jl${lonREF}/${latREF}/${latsec1}/${latsec2}
+paramJ1=${projL}/1:1
+paramJ2=-Jl${lonREF}/${latREF}/${latsec1}/${latsec2}/1:850000
+offset=-C${offsetX}/${offsetY}
+projX=-Jx1:850000
+########################
+
+# Set envelope
+envelope="-R3/5/42.25/43.60" #envelope considered for fitmaxstab v1
+
+#Basemap param
+rose=-T3.13/43.5/0.4i/2
+echelle=-L3.13/43.45/34/10k:"Kilometers":
+
+## PROCESS NODE GEOM ##
+
+# map projection
+awk -F ";" '{print $1,$2,$3}' ${sites} | sed 's/node\[//' | sed 's/\]//' > ${work}/sites.xyz
+log $? "extract xyz file"
+
+gmt mapproject ${work}/sites.xyz $paramJ1 ${envelope} -F -S $offset > ${work}/sites-lamb93.xyz
+log $? "mapproject xyz coords (lamb93)"
+
+join -1 3 -2 3 ${work}/sites-lamb93.xyz ${work}/sites.xyz > ${work}/sites-all.xyz
+log $? "join lam93 and WGS84 coords"
+
+
+# geom computations
+cp ${work}/sites-all.xyz ${work}/sites-all-bis.xyz
+nbsitestot=$(wc -l ${work}/sites-all.xyz | awk '{print $1}')
+awk -v pi=3.14159265359 -v outfile=${outfile} -v nbsitestot=${nbsitestot} '
+	BEGIN { 
+		printf("S1\tS1.x\tS1.y\tS1.lon\tS1.lat\tS2\tS2.x\tS2.y\tS2.lon\tS2.lat\tdist.h\talpha\tlabel\t\n") > outfile ;
+	}
+	FNR==NR {S1[FNR]=$1; S1x[FNR]=$2; S1y[FNR]=$3; S1lon[FNR]=$4; S1lat[FNR]=$5;next}
+	{
+		s1=S1[FNR];s1x=S1x[FNR];s1y=S1y[FNR];s1lon=S1lon[FNR];s1lat=S1lat[FNR];
+		for (i = FNR+1; i <= nbsitestot; i++) {
+			s2=S1[i]; s2x=S1x[i]; s2y=S1y[i]; s2lon=S1lon[i]; s2lat=S1lat[i];
+			dx = s1x-s2x;
+            dy = s1y-s2y;
+            dij = sqrt(dx*dx+dy*dy);
+            cosa = (s2x-s1x)/dij;
+            sina = (s2y-s1y)/dij;
+            atanalpha = atan2(sina/cosa,1);
+            aDeg = -(atanalpha * 180/pi - 90);
+            
+            if (22.5 < aDeg && aDeg <= 67.5) {lab="NE";}
+            else if (67.5 < aDeg && aDeg <= 112.5) {lab="E";}
+            else if (112.5 < aDeg && aDeg <= 157.5) {lab="SE";}
+            else {lab="N";}
+
+
+			printf("%d\t%f\t%f\t%f\t%f\t%d\t%f\t%f\t%f\t%f\t%f\t%f\t%s\n",s1,s1x,s1y,s1lon,s1lat,s2,s2x,s2y,s2lon,s2lat,dij,aDeg,lab) > outfile
+		}
+	}
+' ${work}/sites-all.xyz ${work}/sites-all-bis.xyz
+
+
+## PLOT ##
+
+# gmt ps2raster -E$png_resolution -A -Tg -P ${outfile}.ps
+# log $? "psraster"
+
+# rm ${outfile}.ps
+# log $? "clean ps"
+
+
+
---
+++ GMT/nodesName/nodesName.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#source utilities
+source ./resources/sh/utility.sh
+rightnow
+log "notice" "STARTING... $d"
+
+## PARAMETERS ##
+work="work"
+# bathy="input/sirocco.europe.grd"
+output="output"
+outfile=${output}"/nodename"
+infile="input/megagol2015a-gol.nc"
+
+checkdir $work
+log $? "$work folder clear"
+checkdir $output
+log $? "$outputs folder clear"
+checkoutfile $outfile
+log $? "$outfile clean"
+
+palette="input/shallow-water.cpt"
+rm $palette
+if [ ! -f $palette ] ; then
+	gmt makecpt -Z-500/0/50 -Csealand -G-5000/0 -M > $palette
+	log $? "create palette"
+fi
+
+png_resolution=800
+projection=-JM20c
+gmt gmtset PS_MEDIA a3
+gmt gmtset MAP_ANNOT_ORTHO ver_text
+gmt gmtset MAP_FRAME_TYPE fancy+
+gmt gmtset MAP_FRAME_WIDTH 0.08c
+gmt gmtset FONT_TITLE Helvetica
+gmt gmtset FONT_TITLE 14p
+gmt gmtset MAP_TITLE_OFFSET 0.3c
+gmt gmtset FONT_LABEL 8p
+gmt gmtset MAP_LABEL_OFFSET 0.2c
+gmt gmtset FORMAT_FLOAT_OUT %8.8f
+## END PARAMETERS ##
+
+# Set envelope
+envelope="-R3/5/42.25/43.60" #envelope considered for fitmaxstab v1
+
+#Basemap param
+rose=-T3.13/43.5/0.4i/2
+echelle=-L3.13/43.45/34/10k:"Kilometers":
+
+## PROCESS NODE EXTRACTION ##
+ncks -O -d time,1,1 ${infile} ${work}/foo.nc
+log $? "extract 1 time layer"
+
+#lon 
+ncks -v longitude ${work}/foo.nc |sed '1,11d'  > $work/longitude.ncks
+awk '{print $1"="$2}' $work/longitude.ncks > $work/longitude.ncks2
+awk 'BEGIN { FS = "=" } ;{print $1"	"$3}' $work/longitude.ncks2 > $work/longitude
+log $? "long extraction"
+
+#lat
+ncks -v latitude ${work}/foo.nc |sed '1,11d'  > $work/latitude.ncks
+awk '{print $1"="$2}' $work/latitude.ncks > $work/latitude.ncks2
+awk 'BEGIN { FS = "=" } ;{print $1"	"$3}' $work/latitude.ncks2 > $work/latitude
+log $? "lat extraction"
+
+join $work/longitude $work/latitude > $work/lonlat.xy
+awk  '{print $2"	"$3"   "$1}' $work/lonlat.xy > $work/nodes.xy
+log $? "join node in a lonlat file"
+
+awk 'NR%20==0' $work/nodes.xy > $work/labeltmp.xy
+log $? "extract - only 1 over 20 - label points"
+sed 's/node//g' $work/labeltmp.xy > $work/label.xy
+
+## PLOT ##
+gmt pscoast ${projection} ${envelope} -Df -G#d9bb7a -C#d9bb7a -N1/0.2p,#0000FF,solid  -P -K  >> ${outfile}.ps
+log $? "pscoast"
+
+gmt	psbasemap ${projection} ${envelope} ${echelle} ${rose} -Bf0.1a0.2:longitude:/f0.05a0.1:latitude:/:."Node names":WeSn -P -K -O >> ${outfile}.ps
+log $? "psbasemap"
+
+for i in $(seq 1 3); do
+	hyperslabs="input/hyperslabs-"${i}".dat"
+	gmt	psxy ${hyperslabs} ${projection} ${envelope}  -W2,green -L -P -K -O >> ${outfile}.ps
+	log $? "psxy ${hyperslabs}"
+done
+
+gmt psxy ${envelope} ${projection} $work/nodes.xy -S+0.1 -P -O >> ${outfile}.ps
+log $? "psxy nodes location"
+
+# gmt pstext ${envelope} ${projection} $work/label.xy  -F+a39+f6p,Helvetica,black -P -O >> ${outfile}.ps
+# log $? "pstext labels"
+
+gmt ps2raster -E$png_resolution -A -Tg -P ${outfile}.ps
+log $? "psraster"
+
+rm ${outfile}.ps
+log $? "clean ps"
+
+
+
