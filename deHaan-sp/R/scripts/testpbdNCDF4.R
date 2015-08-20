51594bf2880fa028de91faaefcd6d4ba621f040d
Romain <romain.chailan@gmail.com>
2015-08-20 11:00:24 +0200

    clean test file to post into google group my issue

--- deHaan-sp/R/scripts/testpbdNCDF4.R
+++ deHaan-sp/R/scripts/testpbdNCDF4.R
@@ -1,18 +1,14 @@
-# require(Rmpi)
-library(pbdMPI, quiet=TRUE)
-require(pbdNCDF4)
+require(pbdMPI, quietly = TRUE)
+require(pbdNCDF4, quietly = TRUE)
 init()
 
+prec <- "single"
+missval <- 1.e30
 A <- "../../inputs/ww3/megagol2015a-gol-cleaned3.nc"
-prec="single"
-missval=1.e30
-B <- "toto.nc"
-
-# k <- get.jid(10)
-# my.rank <- comm.rank()
-# comm.cat(my.rank, ":", k, "\n", all.rank=TRUE)
+B <- "B.nc"
 
 
+# READ DIMs FROM A
 in.nc <- nc_open_par(filename = A, readunlim = FALSE)
 node <- ncvar_get(in.nc,"node")
 time <- ncvar_get(in.nc,"time")
@@ -22,11 +18,16 @@ for (i in 1:in.nc$ndim) {
 }
 nc_close(in.nc)
 
+# CREATE NEW FILE B
 dimNode <- ncdim_def("node", "count", node, create_dimvar = TRUE)
 dimTime <- ncdim_def("time", units.time, time, create_dimvar = TRUE)
-transformed.var <- ncvar_def("var_standard","",list(dimNode,dimTime),
-                             missval=missval,prec="float",compression = 9)
+transformed.var <- ncvar_def("var_standard", "", list(dimNode,dimTime), missval=missval, prec="float", compression = 9)
 out.nc <- pbdNCDF4::nc_create_par(B, list(transformed.var), verbose= TRUE)
 nc_close(out.nc)
+
+#
+k <- get.jid(node)
+my.rank <- comm.rank()
+comm.cat(my.rank, ":", k, "\n", all.rank=TRUE)
 
 finalize()
